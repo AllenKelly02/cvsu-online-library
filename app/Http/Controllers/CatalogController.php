46bf517144dc25498c7afa02eac50a0a65aa16f3
AllenKelly02 <allenbaluyut02@gmail.com>
2023-05-31 01:58:41 +0800

    User Navbar Routes

---
+++ app/Http/Controllers/CatalogController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CatalogController extends Controller
+    {
+        public function index()
+        {
+            // Logic for the catalog page
+            // You can return a view or perform any other actions as needed
+            return view('user.navbar.catalog');
+        }
+
+        public function topCollections()
+        {
+            // Logic for the top collections page
+            return view('user.navbar.top-collect');
+        }
+
+        public function newCollections()
+        {
+            // Logic for the new collections page
+            return view('user.navbar.new-collections');
+        }
+
+        public function aboutUs()
+        {
+            // Logic for the about us page
+            return view('user.navbar.about-us');
+        }
+
+        public function askLibrarian()
+        {
+            // Logic for the ask a librarian page
+            return view('user.navbar.ask-librarian');
+        }
+    }
+
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\admin\BooksController;
+use App\Http\Controllers\CatalogController;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\admin\BooksController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,9 +21,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::get('/dashboard', function () {
-//     return view('user.index');
-// })->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', function () {
+    return view('user.index');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
 Route::get('/profile/index', [ProfileController::class, 'index'])->name('profile.index');
@@ -31,9 +32,7 @@ Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.
 
 
 // Route::middleware('role:user')->prefix('user')->as('user.')->group(function (){
-
-
-// });
+    // });
 
 Route::middleware(['auth', 'role:admin'])->prefix('admin')->as('admin.')->group(function() {
     Route::get('/dashboard', [DashboardController::class, 'admin'])
@@ -45,13 +44,18 @@ Route::middleware(['auth', 'role:user'])->prefix('user')->as('user.')->group(fun
     ->name('dashboard');
 });
 
-
+// BROWSE
 Route::get('/books', [BooksController::class, 'books'])->name('books.index');
 
     Route::get('/browse', [BooksController::class, 'browse'])
     ->name('books.browse');
 
-
+// CATALOGS
+Route::get('/', [CatalogController::class, 'index'])->name('catalog');
+Route::get('/top-collect', [CatalogController::class, 'topCollections'])->name('top-collect');
+Route::get('/new-collections', [CatalogController::class, 'newCollections'])->name('new-collections');
+Route::get('/about-us', [CatalogController::class, 'aboutUs'])->name('about-us');
+Route::get('/ask-librarian', [CatalogController::class, 'askLibrarian'])->name('ask-librarian');
 
 
 
