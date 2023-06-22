9576fa51fbf90d2c32e4656b9460a533720b71af
arielrecto <arielrecto@gmail.com>
2023-06-22 13:44:30 +0800

    bugs auth fix

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -18,8 +18,8 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     // public const HOME = '/dashboard';
-    public const HOME = '/dashboard';
-    
+    public const HOME = '/';
+
     public function handleAuthenticationAndRedirection($request)
 {
     $request->authenticate();
