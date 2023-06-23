5165b7c26c332a46845c381260f30c3a92715b88
arielrecto <arielrecto@gmail.com>
2023-06-24 01:29:08 +0800

    unverified-user update

--- resources/views/unverified-accounts/index.blade.php
+++ resources/views/unverified-accounts/index.blade.php
@@ -50,17 +50,6 @@
                                        </form>
                                     </td>
                                     @empty
-<<<<<<< HEAD
-                                    <tr>
-                                        <td colspan="5">
-                                            <div class="alert alert-warning">
-                                                <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg>
-                                                <span>No Item</span>
-                                              </div>
-                                        </td>
-                                    </tr>
-
-=======
                                <tr>
                                 <td colspan="4">
                                     <div class="w-full h-96 flex flex-col items-center justify-center mt-15 ml-20">
@@ -78,7 +67,6 @@
                                     </div>
                                 </td>
                             </tr>
->>>>>>> origin/master
                                 @endforelse
                         </tbody>
                     </table>
