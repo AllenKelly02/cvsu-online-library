7382da041bff32af7e25cfd2e1aa5054a6423fc1
delacruzchristine <delacruzchristine111@gmail.com>
2024-01-13 04:41:55 +0800

    Filter

--- resources/views/user/navbar/catalog.blade.php
+++ resources/views/user/navbar/catalog.blade.php
@@ -28,7 +28,7 @@
     <button class="button-name text-black bg-yellowmain hover:bg-yellowmain focus:ring-4 focus:ring-yellowmain font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center" data-dropdown-toggle="dropdown">Filter by Category <svg class="w-4 h-4 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg></button>
 
     <!-- Dropdown menu -->
-    <div class="hidden bg-white text-base z-50 list-none divide-y divide-gray-100 rounded shadow my-4" id="dropdown">
+    <div class="hidden bg-white text-base z-50 list-none divide-y divide-gray-100 rounded shadow my-4 overflow-y-auto max-h-48 w-32" id="dropdown">
         <ul class="py-1" aria-labelledby="dropdown">
         <li>
             <a href="{{ route('user.catalog') }}" class="w-full block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 z-100">
@@ -45,21 +45,47 @@
         </li>
         </ul>
     </div>
-        <!-- Filter by Book type -->
+    {{-- <button class="button-name text-black bg-yellowmain hover:bg-yellowmain focus:ring-4 focus:ring-yellowmain font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center" data-dropdown-toggle="typedropdown">Filter by Type <svg class="w-4 h-4 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg></button>
 
-        <button class="button-name text-black bg-yellowmain hover:bg-yellowmain focus:ring-4 focus:ring-yellowmain font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center" data-dropdown-toggle="dropdown">Filter by Type <svg class="w-4 h-4 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg></button>
+<!-- Dropdown menu -->
+<div class="hidden bg-white text-base z-50 list-none divide-y divide-gray-100 rounded shadow my-4 overflow-y-auto max-h-48 w-32" id="typedropdown">
+    <ul class="py-1" aria-labelledby="typedropdown">
+        <li>
+            <a href="{{ route('user.catalog') }}" class="w-full block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 z-100">
+                All
+            </a>
+        </li>
+        @foreach ($books as $book)
+        <li>
+            <a href="{{ route('user.catalog') . '?type=book' }}" class="w-full block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 z-100 {{ request()->query('type') === 'book' ? 'bg-gray text-black' : '' }}">
+                {{ $book->type }}
+            </a>
+        </li>
+        
+        @endforeach
+    </ul>
+</div>
 
-        <!-- Dropdown menu -->
-        <div class="hidden bg-white text-base z-50 list-none divide-y divide-gray-100 rounded shadow my-4" id="dropdown">
-            <ul class="py-1" aria-labelledby="typedropdown">
-            <li>
-                <a href="{{ route('user.catalog') }}" class="w-full block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 z-100">
-                    All
-                </a>
-            </li>
-            
-            </ul>
-        </div>
+    <button class="button-name text-black bg-yellowmain hover:bg-yellowmain focus:ring-4 focus:ring-yellowmain font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center" data-dropdown-toggle="typedropdown">Filter by Category <svg class="w-4 h-4 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg></button>
+
+    <!-- Dropdown menu -->
+    <div class="hidden bg-white text-base z-50 list-none divide-y divide-gray-100 rounded shadow my-4 overflow-y-auto max-h-48 w-32" id="typedropdown">
+        <ul class="py-1" aria-labelledby="typedropdown">
+        <li>
+            <a href="{{ route('user.catalog') }}" class="w-full block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 z-100">
+                All
+            </a>
+        </li>
+        <li>
+            <a href="{{ route('user.catalog') . '?type=book' }}" class="w-full block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 z-100 {{ request()->query('category') === 'book' ? 'bg-yellowmain text-black' : '' }}">
+                Book
+            </a>
+            <a href="{{ route('admin.books.index') }}?category=book"
+            class="text-lg px-3 py-1 rounded border text-black border-yellowmain hover:bg-yellowmain hover:text-black {{ request()->query('category') === 'book' ? 'bg-yellowmain text-black' : '' }}">Book</a>
+        </li>
+
+        </ul>
+    </div> --}}
     </div>
     
             <div class="flex flex-wrap -m-4 py-5">
